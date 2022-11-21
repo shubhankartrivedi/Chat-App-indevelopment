61b603e9da4ae51c2098f46a1a46acf0a5f5fe3c
Shubhankar Trivedi <shubhankartrivedi1234@gmail.com>
2022-11-21 09:17:12 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,64 @@
+import "./App.css";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  //Room State
+  const [room, setRoom] = useState("");
+
+  // Messages States
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState([""]);
+
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
+
+  const sendMessage = () => {
+    socket.emit("send_message", { message, room });
+    setMessageReceived((prevM) => [
+      ...prevM,message]);
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived((prevM) => [
+        ...prevM,data.message]);
+    
+    });
+  }, [socket]);
+  return (
+    <div className="App">
+      <input
+        placeholder="Room Number..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
+      <button onClick={joinRoom}> Join Room</button>
+      <input
+        placeholder="Message..."
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+      />
+      <button onClick={sendMessage}> Send Message</button>
+      <h1> Message:</h1>
+      {
+      
+      messageReceived.reverse().map((message) =>{
+
+        
+        return <p>{message}</p>;
+      
+      })
+    }
+    </div>
+  );
+}
+
+export default App;
